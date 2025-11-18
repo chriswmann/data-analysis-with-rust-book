999b12450e479fb68d65d8c31af282a656c5a39e
Chris <8133496+chriswmann@users.noreply.github.com>
2025-11-18 12:21:18 +0000

    Update tracing subscriber format

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,8 +6,15 @@ use tracing_subscriber::prelude::*;
 #[tokio::main]
 async fn main() -> Result<()> {
     // Initialise structured logging with tracing, letting RUST_LOG control verbosity
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false);
+
     tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer())
+        .with(fmt_layer)
         .with(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
